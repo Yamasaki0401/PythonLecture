6a02ab196c14c0fbeb16b0b9999e9a8d00b28e60
Yamasaki <88619524+Yamasaki0401@users.noreply.github.com>
2023-06-16 15:28:31 +0900

    string

---
+++ basic/string_basic.py
@@ -0,0 +1,18 @@
+# 文字列（strings)
+
+print("hello world!!")
+
+#　改行
+print("""
+Hello world!!
+How are you doing?
+""")
+
+print("hello \nworld")
+print("hello \tworld")
+
+print("bash slash n:\\n") #　バックスラッシュを文字列として表示する場合「＼」を使ってエスケープする
+
+print('I\'m fine')
+
+print("hollow" + "world" + "!!") #文字列の連結
