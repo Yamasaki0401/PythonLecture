b608393180ab29ad0f7a616147971e194bfd2286
Yamasaki <88619524+Yamasaki0401@users.noreply.github.com>
2023-06-20 00:10:22 +0900

    formatの使い方

--- basic/assighnement.py
+++ basic/assighnement.py
@@ -1,5 +1,11 @@
 # 変数へ代入：assignment
-a = "Hello"
-world = "World"  # aにHello Worldを代入する  変数名を変更する場合、右クリックでrefactor,renameで同じ変数のものは一度に変更できる
-print(a + world)
+hello = "konnichwa"
+world = "sekai"  # aにHello Worldを代入する  変数名を変更する場合、右クリックでrefactor,renameで同じ変数のものは一度に変更できる
+print(hello + world)
 
+# format
+"hello {}".format("world") #{}に（）内の文字列を入れることができる
+print("{}{}".format(hello, world))
+
+name = "Emily"
+print("Hey,{}!! How are you doing?".format(name))
